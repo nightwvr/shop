22ab9943d53ef2f1d3e08058cc3b5090475b0065
nightwvr <nightwvr@gmail.com>
2025-04-27 16:20:11 +0200

    modules

--- meloni-shop/nuxt.config.ts
+++ meloni-shop/nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt',
+    '@nuxtjs/i18n',
+    '@nuxtjs/color-mode',
+    '@formkit/auto-animate',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/image',
+    '@nuxtjs/sitemap',
+    '@nuxtjs/robots',
+    '@nuxt/icon',
+    '@nuxtjs/google-fonts',
+    '@nuxt/ui',
+    '@nuxtjs/seo'
+  ]
 })
